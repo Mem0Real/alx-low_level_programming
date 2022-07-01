b4ae58e45e3229762eccb2cba6d9213c0ed4b917
Mem0Real <juniourmimo@gmail.com>
2022-07-01 20:48:41 +0000

    .

---
+++ 0x01-variables_if_else_while/100-print_comb3.c~
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+  * main - Print all combinations of two digits
+  * Return: null
+  *
+  * Prints all combinations of two digits separated
+  * by ',' followed by space and the two digits must
+  * be different.
+  */
+
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = '0'; j <= '9'; j++)
+		{
+			if (i < j)
+			{
+				putchar(i);
+				putchar(j);
+				
+				if (i == '8' && j == '9')
+				{
+				}
+
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+/**
+  * main - Prints combinations of 3 digits
+  * Return: null
+  *
+  * This program prints all combinations of
+  * three digits separated by ',' followed
+  * by space. Here the digits must be different
+  * and arranged in ascending order using putchar
+  * function only 6 times without using char.
+  */
+
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = '0'; j <= '9'; j++)
+		{
+			for (k = '0'; k <= '9'; k++)
+			{
+				if (i < j && j < k)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
+					
+					if (i == '7' && j == '8' && k == '9')
+					{
+					}
+
+					else
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
