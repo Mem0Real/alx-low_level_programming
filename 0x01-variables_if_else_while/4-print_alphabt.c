0c9d46810d2654e8eb0b81a1a4324bf25322e806
Mem0Real <juniourmimo@gmail.com>
2022-07-01 17:45:51 +0000

    .

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+  * main - Print alphabet without q or e then new line
+  * Return: int
+  *
+  * This program prints the alphabet without including
+  * the letters 'q' and 'e' then add a new line.
+  */
+
+int main(void)
+{
+	char alphabet[27];
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		alphabet[i] = 'a' + i;
+		if (alphabet[i] != 'q' || alphabet[i] != 'e')
+		{
+			putchar(alphabet[i]);
+		}
+	}
+
+	putchar('\n');
+	alphabet[i] = '\0';
+	return (0);
+}
