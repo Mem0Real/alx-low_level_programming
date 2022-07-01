feeb8757ee03725a0278d10fc8b7f2c1348b9b11
Mem0Real <juniourmimo@gmail.com>
2022-07-01 17:35:04 +0000

    .

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+  * main - Print alphabet in lowercase and uppercase followed by a line
+  * Return: int
+  *
+  * This function prints the alphabet in lowercase,
+  * uppercase then a new line by using 'putchar'
+  * only 3 times.
+  */
+
+int main(void)
+{
+	char alphabet[27];
+	int i;
+	int j;
+
+	for (i = 0; i < 26; i++)
+	{
+		alphabet[i] = 'a' + i;
+		putchar(alphabet[i]);
+	}
+	
+	alphabet[i] = '\0';
+
+	for (j = 0; j < 26; j++)
+	{
+		alphabet[j] = 'A' + j;
+		putchar(alphabet[j]);
+	}
+
+	putchar('\n');
+	alphabet[j] = '\0';
+	return(0);
+}
